29103dc863f781f3f71188a69305dfde09a51941
Carsten Limberger <carsten.limberger@gmx.net>
2019-08-11 00:16:13 +0200

    Add java config for view path

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
   * First experiments with Spring and JSP
   * CSS and JS files included over static files
   * Just using Model and RequestParam in the Controller
+  * Path to JSP views configured in application properties
+
+* v1.2
+  * Added controller that uses ModelMap and ModelAttribute
+  * Path to JSP views configured in a configuration Java class
---
+++ src/main/java/de/cl/playground/file/upload/spring/MyViewConfig.java
@@ -0,0 +1,20 @@
+package de.cl.playground.file.upload.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class MyViewConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
