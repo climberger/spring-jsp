1d8a85445fb6ec3b22b9f648dab493fb0ddfb99f
Carsten Limberger <carsten.limberger@gmx.net>
2019-08-11 00:10:26 +0200

    Second approach

---
+++ src/main/java/de/cl/playground/file/upload/spring/Employee.java
@@ -0,0 +1,12 @@
+package de.cl.playground.file.upload.spring;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private String name;
+    private long id;
+    private String contactNumber;
+
+    // standard getters and setters
+}
---
+++ src/main/java/de/cl/playground/file/upload/spring/EmployeeController.java
@@ -0,0 +1,32 @@
+package de.cl.playground.file.upload.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+public class EmployeeController {
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public ModelAndView showForm() {
+        return new ModelAndView("employeeHome", "employee", new Employee());
+    }
+
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@Valid @ModelAttribute("employee")Employee employee,
+        BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
+            return "error";
+        }
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        return "employeeView";
+    }
+}
